71d0365dbac822fdea5b595cf7d14939cd03bd60
benjaminhope <48177138+benjaminhope@users.noreply.github.com>
2019-04-29 16:55:44 +1000

    Added dragula 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SomeComponent } from './some.component';
+import { DragulaModule } from 'ng2-dragula';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, DragulaModule.forRoot(), DragulaModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
