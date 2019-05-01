e83f9f84c8b4e7e722de0bb3d4a3fac322f78eee
benjaminhope <48177138+benjaminhope@users.noreply.github.com>
2019-05-01 16:14:53 +1000

    Bug Fix
    Problem with file locations and  compiling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,235 +1,37 @@
 import { Component } from '@angular/core';
-import { DragulaService } from 'ng2-dragula';
+import { DragulaModule, DragulaService }   from 'ng2-dragula';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 
+  constructor(private dragulaService: DragulaService) {
 
- constructor(private dragulaService: DragulaService) {
-   this.dragulaService.createGroup("COLUMNS", {
-     direction: 'horizontal',
-     moves: (el, source, handle) => handle.className === "group-handle"
-   });
- }
+      this.dragulaService.createGroup("COLUMNS", {
+        direction: 'vertical',
+        moves: (el, source, handle) => handle.className === "group-handle"
+      });
+    }
 
- public groups:Array<any> = [
-   {
-     name: 'Group A',
-     items: [{name: 'Item A'}, {name: 'Item B'}, {name: 'Item C'}, {name: 'Item D'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   },
-   {
-     name: 'Group B',
-     items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
-   },
-   {
-     name: 'Group C',
-     items: [{name: 'Item W'}, {name: 'Item X'}, {name: 'Item Y'}, {name: 'Item Z'}]
-   }
- ];
+    public groups:Array<any> = [
+      {
+        name: 'Group A',
+        items: [{name: 'Item A'}, {name: 'Item B'}, {name: 'Item C'}, {name: 'Item D'}]
+      },
+      {
+        name: 'Group B',
+        items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
+      },
+      {
+        name: 'Group C',
+        items: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
+      }
+    ];
+
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { SomeComponent } from './some.component';
 import { DragulaModule } from 'ng2-dragula';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, DragulaModule.forRoot(), DragulaModule ],
+  imports:      [ BrowserModule, FormsModule, DragulaModule.forRoot() ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
