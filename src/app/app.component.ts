b77288d49759ec547edba95afc25728583b5161e
benjaminhope <48177138+benjaminhope@users.noreply.github.com>
2019-05-16 19:54:58 +1000

    Add New Slide Added
    Added slide creation + formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,36 +13,61 @@ export class AppComponent {
   task: string;
   saveName: String;
   loadName: String;
+  itemName: String;
+  selectedGroup: String = 'None';
+  selectedGroupID: Integer;
 
-  public groups: Array<any>  = require("src/assets/save/groups.json");
+  public groups: Array<any> = require("src/assets/save/groups.json");
 
   constructor(private dragulaService: DragulaService) {
-      this.dragulaService.createGroup('COLUMNS', {
-        direction: 'horizontal',
-        moves: (el, source, handle) => handle.className === 'header'
-      });
-    }
+    this.dragulaService.createGroup('COLUMNS', {
+      direction: 'horizontal',
+      moves: (el, source, handle) => handle.className === 'header'
+    });
+  }
 
-    newHeader() {
-      this.groups.push({name: this.task, items: [{name: ''}]});
+  newHeader() {
+    this.groups.push({ name: this.task, items: [{ name: '' }] });
+    this.task = '';
+    console.log(this.groups);
+  }
+
+  selectChangeHandler(event: any) {
+    //update the ui
+    this.selectedGroupID = event.target.options.selectedIndex;
+    this.selectedGroup = event.target.value;
+
+  }
+
+  newItem() {
+    try {
+      this.groups[this.selectedGroupID].items.push({ name: this.itemName })
       this.task = '';
-      console.log(this.groups);
     }
-
-    saveButton(){
-      const blob = new Blob([JSON.stringify(this.groups)], {type : 'application/json'});
-      // saveAs(blob, this.saveName + '.json');
-      saveAs(blob, this.saveName + '.json');
-      this.saveName = '';
+    catch (err) {
+      this.selectedGroup = 'Please Select a Group';
     }
+    // this.groups.push({name: this.task, items: [{name: ''}]});
+    // this.task = '';
+    // console.log(this.groups);
+    console.log(this.selectedGroup)
+    console.log(this.groups)
+  }
 
-    loadFile(filePath: string) {
-      console.log(filePath)
-      var fileName = filePath.replace(/^.*[\\\/]/, '')
-      console.log(fileName)
-      this.groups = require("src/assets/save/" + fileName);
+  saveButton() {
+    const blob = new Blob([JSON.stringify(this.groups)], { type: 'application/json' });
+    // saveAs(blob, this.saveName + '.json');
+    saveAs(blob, this.saveName + '.json');
+    this.saveName = '';
+  }
 
-    }
+  loadFile(filePath: string) {
+    console.log(filePath)
+    var fileName = filePath.replace(/^.*[\\\/]/, '')
+    console.log(fileName)
+    this.groups = require("src/assets/save/" + fileName);
+
+  }
 
 
 }
